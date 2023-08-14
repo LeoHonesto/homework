740b7ec27ced89c85fd5ad0daa71b7e3b04fbbe3
LeoHonesto <leohonesto225@gmail.com>
2023-08-14 21:26:56 +0630

    Update daily.dart

--- day11/daily.dart
+++ day11/daily.dart
@@ -3,8 +3,7 @@ void main(){
   print("""HELLO,
 This is your to do list""");
 
-
-  Set toDoList = {};
+List toDoList = [];
 
 while(true){
 print("enter 'A' to Add,'M' to Remove and 'C' for Check");
@@ -14,36 +13,32 @@ if(x == "a"){ print("enter the activity which you want to add");
 String ? z1 = stdin.readLineSync();
 toDoList.add(z1);
 print("now your list is $toDoList");
+print("now you have ${toDoList.length} thing(s) to do");
 } 
 
 else if(x == "m"){ print("enter the activity that you want to remove");
 String ? z2 = stdin.readLineSync();
 toDoList.remove(z2);
-print("now your list is $toDoList"); 
+print("now your list is $toDoList");
+print("now you have ${toDoList.length} thing(s) to do");
 }
 
 else if(x == "c"){ 
   if(toDoList.isEmpty){
   print("your to do list is empty");
-} else {print(toDoList);}
+} else {print(toDoList);
+print("you have ${toDoList.length} thing(s) to do");}
 }
 
 else{ print("ERROR"); break;}
 
-print("enter 'y' for do the (add,check,remove) again and 'b' to break");
+print("enter 'y' for do (add,check,remove) again and 'b' to break");
 String ? y = stdin.readLineSync()?.toLowerCase();
 
 if(y == "b"){print("goodbye"); break;} 
 else if(y == "y"){} 
 else {print("ERROR");break;}
 
-
-
-
- }
-
-
-
-
+}
 
 }
